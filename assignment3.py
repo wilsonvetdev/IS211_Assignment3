ca604431e95afc43d9473b449f53a896cfcc1c5b
wilsonvetdev <wilsongch92@gmail.com>
2021-02-12 18:42:19 -0500

    able to download data and write to csv file

--- assignment3.py
+++ assignment3.py
@@ -1,14 +1,43 @@
 import argparse
-# other imports go here
+import urllib.request
+import logging
+import ssl
+import csv, re
+import sys
+import pprint
+
+#  url = http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv
+
+def download_data(url):
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+        
+        with open('img_data.csv', 'w') as img_file:
+            data = data.decode('utf-8').splitlines()
+
+            writer = csv.writer(img_file, delimiter = '\n')
+
+            for line in data:
+            # writerow() needs a list of data to be written, so split at all empty spaces in the line 
+                writer.writerow(re.split('\n', line))
 
 
-def main(url):
-    print(f"Running main with URL = {url}...")
+data = download_data('http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv')
+
+with open('img_data.csv', newline='') as img_file:
+    data_reader = csv.reader(img_file, delimiter="'")
+    for line in data_reader:
+        print(line)
+
+# def main(url):
+#     print(f"Running main with URL = {url}...")
 
 
-if __name__ == "__main__":
-    """Main entry point"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
-    args = parser.parse_args()
-    main(args.url)
+# if __name__ == "__main__":
+#     """Main entry point"""
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
+#     args = parser.parse_args()
+#     main(args.url)
